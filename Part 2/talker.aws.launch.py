0b78c9b12e9e7a05b837778e00bc2a875988aa5f
Simeon ADEBOLA <39528961+SimeonOA@users.noreply.github.com>
2022-08-10 11:32:51 -0700

    Update talker.aws.launch.py

--- Part 2/talker.aws.launch.py
+++ Part 2/talker.aws.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
     """Talker example that launches the listener on AWS."""
     ld = fogros2.FogROSLaunchDescription()
     machine1 = fogros2.AWSCloudInstance(
-        region="us-west-1", ec2_instance_type="t2.micro", ami_image=ami_image()
+        region="us-west-2", ec2_instance_type="t2.micro", ami_image=ami_image()
     )
 
     listener_node = Node(
